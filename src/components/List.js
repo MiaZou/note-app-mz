d73ec777a78ebf31d46d46acf6789b000761712e
MiaaaaaaZ <mirongzou@gmail.com>
2020-01-15 19:44:19 +0000

    added components:nav/list/note

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends React.Component {
+  render() {
+    return (
+      <div className="list-container">
+        List component
+      </div>
+    );
+  }
+}
+
+export default List;
